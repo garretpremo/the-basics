c744b38f3ed4edccf7b8bdb009205b561fff8d8e
Garret Premo <garretpremo@gmail.com>
2024-02-27 09:54:06 -0500

    express server with first GET request, add postgres to docker-compose

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,10 @@ const app = express();
 // Expose the frontend build
 app.use('/', express.static('public/browser'));
 
+app.get('/hello', (req, res) => {
+    res.send('goodbye');
+});
+
 // Start the server
 const port = process.env['PORT'] || 8156;
 app.listen(port, () => {
